78d02a6a2ec127b0954b6080f463e29757570806
HuyVoTuan <huy.vt00578@sinhvien.hoasen.edu.vn>
2024-08-06 17:13:52 +0700

    fix: back-to-top-buttons.jsx

--- src/components/back-to-top-button.jsx
+++ src/components/back-to-top-button.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
 import debounce from '../helpers/debounce';
+import { useState, useEffect, useCallback } from 'react';
 
 // UI lib
 import { Button, Tooltip } from 'antd';
@@ -11,30 +11,47 @@ const BACK_TO_TOP_LIMIT = 300;
 export default function BackToTopButton({ scrollRef }) {
   const [backToTopButton, setBackToTopButton] = useState(false);
 
+  // Scroll to top
+  const scrollTopHandler = () => {
+    scrollRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  /* WAY 1: THIS WAY IS SLOWER */
+  // useEffect(() => {
+  //   const handleScroll = debounce(() => {
+  //     const currentScrollTop = scrollRef.current.scrollTop;
+  //     setBackToTopButton(currentScrollTop > BACK_TO_TOP_LIMIT);
+  //   }, 200);
+
+  //   const scrollTopDiv = scrollRef.current;
+  //   scrollTopDiv.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     scrollTopDiv.removeEventListener('scroll', handleScroll);
+  //   };
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  /* WAY 2: THIS WAY IS FASTER */
+  // Callback hooks
+  const handleScroll = useCallback(() => {
+    const debouncedHandleScroll = debounce(() => {
+      const currentScrollTop = scrollRef.current.scrollTop;
+      setBackToTopButton(currentScrollTop > BACK_TO_TOP_LIMIT);
+    }, 200); // Adjust the debounce delay as needed
+
+    debouncedHandleScroll();
+  }, [scrollRef]);
+
+  // Effect hooks
   useEffect(() => {
-    const handleScroll = debounce(() => {
-      const scrollTopDivPixels = scrollRef.current.scrollTop;
-
-      if (scrollTopDivPixels > BACK_TO_TOP_LIMIT) {
-        setBackToTopButton(true);
-      } else {
-        setBackToTopButton(false);
-      }
-    }, 200);
-
     const scrollTopDiv = scrollRef.current;
     scrollTopDiv.addEventListener('scroll', handleScroll);
 
     return () => {
       scrollTopDiv.removeEventListener('scroll', handleScroll);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // Scroll to top function handler
-  const scrollTopHandler = () => {
-    scrollRef.current.scrollTo({ top: 0, behavior: 'smooth' });
-  };
+  }, [handleScroll, scrollRef]);
 
   return (
     <>
